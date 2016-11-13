f7b63f6a25ca3fb9b698cd95329d9999d2873969
spinclick <spinclick@users.noreply.github.com>
2016-11-12 22:40:21 -0500

    Update main.ml

--- main.ml
+++ main.ml
@@ -1,43 +1,26 @@
-(*Log.set_info [];;*)
-
-exception Unsupported ;;
+Log.set_info [];;
 
 let print_keys_exit () =
   print_endline "Valid keys are: ";
-  List.iter (fun t -> Printf.printf "   %s\n" (fst t)) World.wld_header_read_seq;
+  List.iter (fun t -> Printf.printf "   %s\n" (fst t)) World.header_pairs;
   exit 1;;
 
 let print_usage_exit () =
   print_endline "Usage: ./wldquery [wldpath] [key1 key2 ...]";
   print_keys_exit ();;
 
-let print_assoc_item seq_type b = 
-  let open Util in
-  let open World in
-  begin
-    match seq_type with
-      | Bool -> print_string (string_of_bool @@ bool_of_bytes b)
-      | Byte -> print_int @@ int_of_char (Bytes.get b 0) 
-      | Int16 -> print_int @@ int16_of_bytes b
-      | Int32 -> Printf.printf "%ld" @@ int32_of_bytes b
-      | Int64 -> Printf.printf "%Ld" @@ int64_of_bytes b
-      | Array (read_type_t, array_size_t) -> print_string "Warning: Printing array values not implemented"
-      | String -> print_string @@ string_of_bytes b 
-      | Single -> print_float @@ single_of_bytes b
-      | Double -> Printf.printf "%Ld" @@ double_of_bytes b
-    end;
-    print_newline () ;;
+let wld_header_of_path path = 
+  let in_ch = open_in_bin path in
+  World.read_header in_ch ;;
 
 let main () =  
   try begin
     let assoc = List.assoc in
     let inpath = Sys.argv.(1) in
     let keys = List.tl @@ List.tl @@ Array.to_list Sys.argv in
-    let header = World.wld_header_of_path inpath in
-    let header_def = World.wld_header_read_seq in
+    let header = wld_header_of_path inpath in
     if (List.length keys) = 0 then raise Not_found else begin
-      (*List.iter print_endline keys;*)
-      List.iter (fun key -> print_assoc_item (assoc key header_def) (assoc key header)) keys
+      List.iter (fun key -> World.print_header_pair (key,(assoc key header))) keys
     end
   end
   with Invalid_argument i -> print_usage_exit ()
