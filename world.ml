9e9f5c58d99270f10b1f451db9a46bbd06ab152c
spinclick <spinclick@users.noreply.github.com>
2016-11-12 22:40:05 -0500

    Import overhauled world.ml

--- world.ml
+++ world.ml
@@ -1,63 +1,87 @@
-type array_size_t =
-  | Static of int (* fixed number of elements *)
-  | Variable
-;;
+open Extensions.Pervasives ;; 
 
-type read_type_t = 
-  | Bool
-  | Byte 
-  | Int16
-  | Int32
-  | Int64
-  | Array of (read_type_t * array_size_t)
-  | String
-  | Single
-  | Double
-;;
+exception Wld_version_unsupported of string ;;
+exception Read_type_unsupported of string ;;
+exception Unknown_array_size_type of string ;;
+let supported_wld_version = 177;;
+
+type array_size =
+  | Static of int (* fixed number of elements *)
+  | Variable of string (* Use string as key to the number of elements *) ;;
+
+type read_type = 
+  | Bool | Byte 
+  | Int16 | Int32 | Int64
+  | Single | Double
+  | Array of (read_type * array_size)
+  | String ;;
+
+type header_value = 
+  | HBool of bool 
+  | HByte of int
+  | HInt of int64
+  | HFloat of float
+  | HArray of header_value array
+  | HString of string ;;
+
+type header_pair = (string * header_value) ;;
+type header = header_pair list ;;
+
+type tiles = int array array ;;
+
+type position = 
+  { x : int ;
+    y : int } ;;
+
+type item = 
+ { id : int ;
+   prefix : int } ;;
+
+type chest = 
+  { pos : position ;
+    name : string ;
+    items : item array } ;;
+
+type sign = 
+  { pos : position ;
+    text : string } ;;
+
+type entity = 
+  { pos: position ;
+    id: int; } ;;
+
+type npc = 
+  { active : bool ;
+    name : string ;
+    displayName : string ;
+    positionX : string ;
+    positionY : string ;
+    homeless : bool ;
+    homeTileX : int ;
+    homeTileY : int } ;;
+
+type pressure_plate = { pos: position } ;;
+
+type world = 
+  { header: header ;
+    tiles: tiles ;
+    chests: chest array ;
+    signs: sign array ;
+    npcs : npc array ;
+    entities: entity array ;
+    pressure_plates: pressure_plate array } ;;
 
 let rec byte_size_of_type = function
   | Bool | Byte -> 1
   | Int16 -> 2
   | Int32 | Single -> 4
   | Int64 | Double -> 8
-  | String -> 0
-  | Array (read_type, array_size) -> 
-      (byte_size_of_type read_type) * 
-      begin 
-        match array_size with
-          Static i -> i
-          | _ -> 0
-      end
-;;
+  | Array (t, Static array_size) -> 
+      (byte_size_of_type t) * array_size 
+  | String | _ -> 0 ;;
 
-exception Read_too_few_bytes of bytes ;;
-exception Wld_version_unsupported of string ;;
-let supported_wld_version = 156;;
-
-let single_read fd len =
-  let buf = Bytes.create len in
-  let rec _single_read read = 
-    let r = Unix.read fd buf read (len - read) in
-    match r with
-      | _ when (r + read) = len -> buf
-      | 0 -> raise (Read_too_few_bytes buf)
-      | _ -> (_single_read (r + read))
-  in
-  _single_read 0
-;;
-
-let read_int16 fd = Util.int16_of_bytes @@ single_read fd 2 ;;
-let read_int32 fd = Util.int32_of_bytes @@ single_read fd 4 ;; 
-let read_single fd = Util.single_of_bytes @@ single_read fd 4 ;; 
-let read_int64 fd = Util.int64_of_bytes @@ single_read fd 8 ;;
-let read_byte fd = Util.byte_of_bytes @@ single_read fd 1 ;;
-let read_bool fd = Util.bool_of_bytes @@ single_read fd 1 ;;
-let read_string fd = 
-  let len = Util.byte_at (single_read fd 1) 0 in
-  single_read fd len
-;;
-
-let wld_header_read_seq = [
+type header_read_pairs_t = (string * read_type ) list ;;
+let header_pairs = [
    ("version", Int32); 
    ("relogic", Int64);
    ("revision", Int32);
@@ -65,7 +89,7 @@ let wld_header_read_seq = [
    ("_num_position", Int16);
    ("positions", Array (Int32, Static 10));
    ("_num_importance", Int16);
-   ("importance", Array (Bool, Static 53));
+   ("importance", Array (Bool, Static 58));
 
    ("world_name", String);
    ("world_id", Int32);
@@ -146,7 +170,7 @@ let wld_header_read_seq = [
    ("num_clouds", Int16);
    ("wind_speed", Single);
    ("_num_angler_finished", Int32);
-   ("angler_who_finished_today", Array (String, Variable));
+   ("angler_who_finished_today", Array (String, Variable "_num_angler_finished"));
    ("saved_angler", Bool);
    ("angler_quest", Int32);
    ("saved_stylist", Bool);
@@ -154,7 +178,7 @@ let wld_header_read_seq = [
    ("invasion_size_start", Int32);
    ("temp_cultist_delay", Int32);
    ("_num_npc_killed", Int16);
-   ("npc_kill_count", Array (Int32, Variable));
+   ("npc_kill_count", Array (Int32, Variable "_num_npc_killed"));
    ("fast_forward_time", Bool);
    ("downed_fishron", Bool);
    ("downed_martians", Bool);
@@ -175,78 +199,89 @@ let wld_header_read_seq = [
    ("tower_active_vortex", Bool);
    ("tower_active_nebula", Bool);
    ("tower_active_stardust", Bool);
-   ("lunar_apocalypse_is_up", Bool) ] ;;
+   ("lunar_apocalypse_is_up", Bool);
+   (* v 170 *)
+   ("temp_party_manual", Bool);
+   ("temp_party_genuine", Bool);
+   ("temp_party_cooldown", Int32);
+   ("_num_celebrating", Int32);
+   ("temp_party_celebrating_npcs", Array (Int32, Variable "_num_celebrating"));
+   (* v 174*)
+   ("temp_sandstorm_happening", Bool);
+   ("temp_sandstorm_time_left", Int32);
+   ("temp_sandstorm_severity", Single);
+   ("temp_sandstorm_intended_severity", Single) ] ;;
 
-(* Load header *)
-let load_wld_header fd = 
-  let read num_bytes = single_read fd num_bytes in
-  let read_n_strings n =
-    let byte_strings = Extensions.List.make_fn (fun () -> read_string fd) n in
-    let buffer = Buffer.create 300 in
-    List.iter (Buffer.add_bytes buffer) byte_strings ; (* Should we null-terminate these? *)
-    Buffer.to_bytes buffer;
+let sprintf = Printf.sprintf ;;
+let read_header_from_pair in_chan (previously_read : header) pair : header =
+  let rec data_of_pair = function
+    | ("version", Int32) -> 
+        let v = (read_int32 in_chan) in
+        if (Int32.to_int v) != supported_wld_version
+          then 
+            raise @@ Wld_version_unsupported 
+              (sprintf "Wld version is %ld, I only support %d" v supported_wld_version)
+          else ("version", HInt (Int64.of_int32 v))
+    | (key, Int64) -> (key, HInt (read_int64 in_chan))
+    | (key, Int32) -> (key, HInt (Int64.of_int32 (read_int32 in_chan)))
+    | (key, Int16) -> (key, HInt (Int64.of_int (read_int16 in_chan)))
+    | (key, Bool)  -> (key, HBool (read_bool in_chan))
+    | (key, Byte)  -> (key, HByte (read_byte in_chan))
+    | (key, String) -> (key, HString (read_pascal_string in_chan))
+    | (key, Single) -> (key, HFloat (read_single in_chan))
+    | (key, Double) -> (key, HFloat (read_double in_chan))
+    | (key, Array (Int32, Static n)) -> begin
+       let int32s = read_int32_array in_chan n in
+       (key, HArray (Array.map (fun i -> HInt (Int64.of_int32 i)) int32s))
+     end
+    | (key, Array (Bool, Static n)) -> begin
+       let bools = read_bool_array in_chan n in
+       (key, HArray (Array.map (fun b -> HBool b) bools))
+     end
+    | (key, Array (String, Static n)) -> begin
+       let strings = read_string_array in_chan n in
+       (key, HArray (Array.map (fun b -> HString b) strings))
+     end
+    | (key, Array (t, Variable prior_key)) ->  begin  (* Convert Variable types to Static*)
+       let amount = 
+         match (List.assoc prior_key previously_read) with
+           | HInt d -> Int64.to_int d
+           | _ -> raise @@ Unknown_array_size_type key
+       in
+       (data_of_pair (key, Array (t, Static amount)))
+     end
+    | (key, _) -> raise @@ Read_type_unsupported key 
   in
-  let rec read_header read_seq bytelst = 
-    match read_seq with
-      | [] -> bytelst
-      | (s, Bool) :: tl -> read_header tl ((s,(read 1)) :: bytelst)
-      | (s, Byte) :: tl -> read_header tl ((s,(read 1)) :: bytelst)
-      | (s, Int16) :: tl -> read_header tl ((s,(read 2)) :: bytelst)
-      | (s, Int32) :: tl -> read_header tl ((s,(read 4)) :: bytelst)
-      | (s, Single) :: tl -> read_header tl ((s,(read 4)) :: bytelst)
-      | (s, Int64) :: tl -> read_header tl ((s,(read 8)) :: bytelst)
-      | (s, Double) :: tl -> read_header tl ((s,(read 8)) :: bytelst)
-      | (s, String) :: tl -> begin
-            let strsz = int_of_char @@ Bytes.get (read 1) 0 in
-            read_header tl ((s,(read strsz)) :: bytelst)
-          end
-      | (s, Array (t, array_sz)) :: tl -> begin
-        let elements =     (* How many elements has this array? *)
-          match array_sz with 
-            | Variable -> Util.int_of_bytes @@ snd (List.hd bytelst)  (* size of last read variable *)
-            | Static sz -> sz 
-        in
-        match t with
-          | String ->  (* Array of strings? *)
-              read_header tl ((s,(read_n_strings elements)) :: bytelst)
-          | t -> begin (* Array of other types? *)
-              let readsize = (elements * (byte_size_of_type t)) in
-              read_header tl ((s,(read readsize)) :: bytelst)
-          end
-        end
+  Log.printf Log.Debug "Reading '%s'\n" (fst pair);
+  ((data_of_pair pair) :: previously_read) ;;
+
+let read_header inch : header = 
+  List.fold_left 
+    (read_header_from_pair inch)
+    []
+    header_pairs ;;
+
+let print_header_pair p =
+  let open Printf in
+  let open Array in
+  let rec string_of_header_data = function
+    | HBool b -> sprintf "%b" b
+    | HByte i -> sprintf "%d" i
+    | HInt i64 -> sprintf "%Ld" i64
+    | HFloat f -> sprintf "%f" f
+    | HArray ar -> "[" ^ (String.concat "," (List.map string_of_header_data (to_list ar))) ^ "]"
+    | HString str -> "\"" ^ str ^ "\""
   in
-
-  let header = read_header wld_header_read_seq [] in
-
-  List.iter (fun x -> Log.label_bytes Log.Debug (fst x) (snd x)) (List.rev header);
-  Log.infoln "Loaded header";
-  Log.file_offset Log.Debug fd;
-
-  header;
+  printf "%s: %s\n" (fst p) (string_of_header_data (snd p))
 ;;
 
-let wld_header_of_fd fd = 
-  let header = load_wld_header fd in
-  let version = Util.int_of_bytes @@ List.assoc "version" header in
-  let _ = 
-    if (supported_wld_version = version) then ()
-    else 
-    begin
-      let errstr = 
-        Printf.sprintf "World version must be %d, this wld has version %d" 
-                        supported_wld_version version 
-      in
-      raise (Wld_version_unsupported errstr)
-    end
-  in
-  header
-;;
-
-let wld_header_of_path pathstr = 
-  (*Log.noticef "Reading world file '%s'\n" pathstr;*)
-  let open Unix in
-  let fd = openfile pathstr [O_RDONLY] 0 in
-  let header = wld_header_of_fd fd in
-  close fd;
-  header
-;;
+let read_tiles in_ch = () ;;
+let read_chests in_ch = () ;;
+let read_signs in_ch = () ;;
+let read_npcs in_ch = () ;;
+let read_entities in_ch = () ;;
+let read_pressure_plates in_ch = () ;;
+let read_world in_ch = ()
+let world_of_file path = () ;;
+let world_of_buffer bytes = () ;;
+let save_world_to_file world = () ;;
